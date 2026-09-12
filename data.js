0f45c0386b0a944c656d626d4842331b636b8b39
nawaf1998f-ops <nawaf1998f@gmail.com>
2026-09-13 00:35:12 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,44 @@
 const products = [
   {
-    name: "مكيف سبليت 18000 وحدة",
-    category: "مكيفات",
-    price: 1899
+    id: 1,
+    name: "مكيف سبليت 18 وحدة",
+    category: "ac",
+    prices: [
+      { store: "متجر 1", price: 1899 },
+      { store: "متجر 2", price: 1949 },
+      { store: "متجر 3", price: 1999 }
+    ]
   },
   {
-    name: "مكيف سبليت 24000 وحدة",
-    category: "مكيفات",
-    price: 2399
+    id: 2,
+    name: "مكيف سبليت 24 وحدة",
+    category: "ac",
+    prices: [
+      { store: "متجر 1", price: 2399 },
+      { store: "متجر 2", price: 2449 },
+      { store: "متجر 3", price: 2499 }
+    ]
   },
   {
-    name: "مكيف شباك 18000 وحدة",
-    category: "مكيفات",
-    price: 1299
+    id: 3,
+    name: "مكيف شباك 18 وحدة",
+    category: "ac",
+    prices: [
+      { store: "متجر 1", price: 1299 },
+      { store: "متجر 2", price: 1349 },
+      { store: "متجر 3", price: 1399 }
+    ]
   },
   {
-    name: "مكيف شباك 24000 وحدة",
-    category: "مكيفات",
-    price: 1599
+    id: 4,
+    name: "مكيف شباك 24 وحدة",
+    category: "ac",
+    prices: [
+      { store: "متجر 1", price: 1599 },
+      { store: "متجر 2", price: 1649 },
+      { store: "متجر 3", price: 1699 }
+    ]
   }
 ];
 
+window.products = products;
