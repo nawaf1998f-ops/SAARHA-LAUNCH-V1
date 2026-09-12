55660d0b44120ef1586224ceb38de3e7026fb8bd
nawaf1998f-ops <nawaf1998f@gmail.com>
2026-09-12 23:58:19 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,24 @@
+const products = [
+  {
+    name: "مكيف سبليت 18000 وحدة",
+    category: "مكيفات",
+    price: 1899
+  },
+  {
+    name: "مكيف سبليت 24000 وحدة",
+    category: "مكيفات",
+    price: 2399
+  },
+  {
+    name: "مكيف شباك 18000 وحدة",
+    category: "مكيفات",
+    price: 1299
+  },
+  {
+    name: "مكيف شباك 24000 وحدة",
+    category: "مكيفات",
+    price: 1599
+  }
+];
+
+export default products;
